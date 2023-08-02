a36b75a1f48d688e21ceffa5e9c0b8ab2dfd7e6d
devgiants75 <devgiants75@naver.com>
2023-08-02 11:20:39 +0900

    20230802

--- python-bt0720-lsa/0802/e_practice02.py
+++ python-bt0720-lsa/0802/e_practice02.py
@@ -1,7 +1,13 @@
 # "message.txt"라는 파일을 생성하고
 # , 그 안에 "Hello, Python!" 이라는 메시지를 쓰는 파이썬 프로그램을 작성
+with open('message.txt', 'wt', encoding='utf-8') as file:
+    file.write('Hello, Python!')
 
 # "message.txt" 파일에 "Learning file I/O is fun!"이라는
 # 새로운 줄을 추가하는 파이썬 프로그램을 작성
+with open('message.txt', 'a', encoding='utf-8') as file:
+    file.write("Learning file I/O is fun!")
 
 # "message.txt" 파일의 내용을 읽어서 콘솔에 출력하는 파이썬 프로그램을 작성
+with open('message.txt', 'r', encoding='utf-8') as file:
+    print(file.read())
