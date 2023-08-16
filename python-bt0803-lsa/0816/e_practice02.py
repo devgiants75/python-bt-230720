ccef708296816053e6528f5b988a2ee8fc3311a3
devgiants75 <devgiants75@naver.com>
2023-08-16 11:26:30 +0900

    20230816

--- python-bt0803-lsa/0816/e_practice02.py
+++ python-bt0803-lsa/0816/e_practice02.py
@@ -18,34 +18,32 @@ import numpy as np
 # sheet_name="Sheet1"은 불러올 시트의 이름
 # header=None은 파일의 첫 번째 행을 컬럼명으로 사용하지 않겠다는 의미
 # index_col=None은 파일의 첫 번째 열을 인덱스로 사용하지 않겠다는 의미
-dataframe = pd.read_excel('./python0803.xlsx', sheet_name='Sheet1', header=None, index_col=None)
+dataframe = pd.read_excel('D:\\python-bt0720-lsa-git\\python-bt0803-lsa\\python0803.xlsx', sheet_name='Sheet1', header=None, index_col=None)
 
 # 데이터 추출 및 변환
 # dataframe.iloc[0, 1:13]는 데이터프레임의 첫 번째 행과 2번째부터 13번째 열까지의 데이터를 선택
-x축 = dataframe.iloc[0, 1:13].to_numpy # 선택한 행렬 데이터를 array로 변환
-y축2020 = dataframe.iloc[1, 1:13].to_numpy
-y축2021 = dataframe.iloc[2, 1:13].to_numpy
-y축2022 = dataframe.iloc[3, 1:13].to_numpy
-
-소비카테고리 = dataframe.iloc[5, 1:5].to_numpy
-이번달소비금액 = dataframe.iloc[6, 1:5].to_numpy
-이번달소비금액합계 = np.nansum(이번달소비금액, dtype='float16')
-
-평균2020 = np.nanmean(y축2020, dtype='float16')
-평균2021 = np.nanmean(y축2021, dtype='float16')
-평균2022 = np.nanmean(y축2022, dtype='float16')
+x축 = dataframe.iloc[0, 1:13].to_numpy()  # 선택한 행렬 데이터를 array 로 변환
+y축2020 = dataframe.iloc[1, 1:13].to_numpy()
+y축2021 = dataframe.iloc[2, 1:13].to_numpy()
+y축2022 = dataframe.iloc[3, 1:13].to_numpy()
+소비카테고리 = dataframe.iloc[5, 1:5].to_numpy()
+이번달소비금액 = dataframe.iloc[6, 1:5].to_numpy()
+이번달소비금액합계 = np.nansum(이번달소비금액, dtype="float16")
+평균2020 = np.nanmean(y축2020, dtype="float16")
+평균2021 = np.nanmean(y축2021, dtype="float16")
+평균2022 = np.nanmean(y축2022, dtype="float16")
 
 #! 엑셀이 없는 경우: 데이터 하드 코딩
-x축 = ["1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"]
-y축2020 = [38, 50, 25, 35, 47, 33, 112, 72, 23, 32, 46, 82]
-y축2021 = [48, 60, 35, 45, 57, 43, 122, 82, 33, 42, 56, 92]
-y축2022 = [58, 70, 45, 55, 67, 53, 132, 92, 43, 52, 66, 102]
-소비카테고리 = ["식비", "카페", "영화", "취미생활"]
-이번달소비금액 = [32, 9, 3, 7]
-이번달소비금액합계 = 85
-평균2020 = 57
-평균2021 = 66
-평균2022 = 78
+# x축 = ["1월", "2월", "3월", "4월", "5월", "6월", "7월", "8월", "9월", "10월", "11월", "12월"]
+# y축2020 = [38, 50, 25, 35, 47, 33, 112, 72, 23, 32, 46, 82]
+# y축2021 = [48, 60, 35, 45, 57, 43, 122, 82, 33, 42, 56, 92]
+# y축2022 = [58, 70, 45, 55, 67, 53, 132, 92, 43, 52, 66, 102]
+# 소비카테고리 = ["식비", "카페", "영화", "취미생활"]
+# 이번달소비금액 = [32, 9, 3, 7]
+# 이번달소비금액합계 = 85
+# 평균2020 = 57
+# 평균2021 = 66
+# 평균2022 = 78
 
 print(x축)
 print(y축2020)
@@ -58,3 +56,18 @@ print(평균2020)
 print(평균2021)
 print(평균2022)
 
+import matplotlib.pyplot as plt
+
+figure = plt.figure()
+
+axes = figure.add_subplot(3, 1, 1)
+
+axes.plot(x축, y축2020, marker='o', label='2020년')
+axes.plot(x축, y축2021, marker='^', label='2021년')
+axes.plot(x축, y축2022, marker='s', label='2022년')
+
+plt.legend()
+plt.xlabel('월') 
+plt.ylabel('카드사용금액')
+
+plt.show()
